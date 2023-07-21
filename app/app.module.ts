17284ccda9f8c27680d660dfd29956912a084a28
197CT22160 - Phạm Nhật Hào - K25T-IT5 <hao.197ct22160@vanlanguni.vn>
2023-07-21 21:43:40 +0700

    3 commit

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { SuggestedProductsComponent } from './suggested-products/suggested-products.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    SuggestedProductsComponent,
+    HomeComponent,
+    ProductsComponent,
+    ProductDetailsComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule
